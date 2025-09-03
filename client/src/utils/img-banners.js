8758c9f542532a7bdda246e6bd7b788ee19a79aa
Mafretd Joya <mafretd@gmail.com>
2025-09-03 16:52:38 -0500

    Provisional solution to serve the path of the images and other static resources.

---
+++ client/src/utils/img-banners.js
@@ -0,0 +1,23 @@
+// TODO: Create an automated way to serve the path and info of the resourses
+// from the backend
+
+export const bannerImg = [
+    "/img/banners/878_generated.jpg",
+    "/img/banners/887_generated.jpg",
+    "/img/banners/904_generated.jpg",
+];
+
+export const books = [
+    "/img/books/new-books/978628753983.GIF",
+    "/img/books/new-books/978628753998.GIF",
+    "/img/books/new-books/978628763890.GIF",
+    "/img/books/new-books/978628766968.GIF",
+    "/img/books/new-books/978628779400.GIF",
+    "/img/books/new-books/978841286291.GIF",
+    "/img/books/new-books/978841824603.GIF",
+    "/img/books/new-books/978842499939.GIF",
+    "/img/books/new-books/978844604231.GIF",
+    "/img/books/new-books/978844934030.GIF",
+    "/img/books/new-books/978847506809.GIF",
+    "/img/books/new-books/978849187284.GIF",
+];
