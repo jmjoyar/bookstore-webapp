6b034545242cb555b9c12ae8984656a2d6cf3ed5
Mafretd Joya <mafretd@gmail.com>
2025-09-03 12:52:56 -0500

    Initial commit

---
+++ backend/src/test/java/com/sc/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sc.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
